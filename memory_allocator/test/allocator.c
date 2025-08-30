36c7b5620aaf9eed29a4a2055d903a9ef3915635
hisyamsk <hisyamskurniawan18@gmail.com>
2025-08-30 16:13:03 +0700

    test: add calloc and realloc unit tests

--- memory_allocator/test/allocator.c
+++ memory_allocator/test/allocator.c
@@ -94,7 +94,8 @@ TEST(free_mem_unallocated_heap_logs_error) {
     free_mem(pp - 10);
 
     char *out = capture_stderr_end(s, r);
-    ASSERT_STR_MATCH("free_mem for unallocated heap address log error", out, "heap corruption");
+    ASSERT_STR_MATCH("free_mem for unallocated heap address should log error", out,
+                     "heap corruption");
     free(out);
 }
 
@@ -123,4 +124,92 @@ TEST(use_after_free) {
     memset(expected, 0xAA, 10);
 
     ASSERT_MEM_EQUAL("memory should be filled with poison for UAF", expected, p, 10);
+}
+
+TEST(calloc_mem_success) {
+    int *p = calloc_mem(10, sizeof(int));
+    ASSERT_NOT_NULL("p should not be null", p);
+
+    int expected[10] = {0};
+    ASSERT_MEM_EQUAL("p values should be set to zero", expected, p, 10 * sizeof(int));
+}
+
+TEST(realloc_mem_success) {
+    char *p1 = alloc_mem(11);
+    ASSERT_NOT_NULL("p should not be null", p1);
+    strcpy(p1, "ABCDEFGHIJ\0");
+
+    char *p2 = realloc_mem(p1, 20);
+    ASSERT_STR_EQUAL("p2 should have the same content from p1", "ABCDEFGHIJ\0", p2, 11);
+}
+
+TEST(realloc_zero_size) {
+    char *p = alloc_mem(32);
+    ASSERT_NOT_NULL("p should not be null", p);
+
+    char *p2 = realloc_mem(p, 0);
+    ASSERT_NULL("p2 should be null", p2);
+
+    char expected[32];
+    memset(expected, 0xAA, 32);
+
+    ASSERT_MEM_EQUAL("realloc_mem should free and posion memory", expected, p, 32);
+}
+
+TEST(realloc_mem_stack_address_logs_error) {
+    int s, r;
+    capture_stderr_start(&s, &r);
+
+    char *a = "hello\0";
+    realloc_mem(a, 10);
+
+    char *out = capture_stderr_end(s, r);
+    ASSERT_STR_MATCH("realloc_mem for stack address should log error", out, "out of heap bounds");
+    free(out);
+}
+
+TEST(realloc_mem_offset_pointer_logs_error) {
+    int s, r;
+    capture_stderr_start(&s, &r);
+
+    char *p = alloc_mem(32);
+    ASSERT_NOT_NULL("p should not be null", p);
+
+    realloc_mem(p + 3, 40);
+
+    char *out = capture_stderr_end(s, r);
+    ASSERT_STR_MATCH("realloc_mem for offset pointer should log error", out, "heap corruption");
+    free(out);
+}
+
+TEST(realloc_mem_unallocated_heap_logs_error) {
+    int s, r;
+    capture_stderr_start(&s, &r);
+
+    char *p = alloc_mem(32);
+    ASSERT_NOT_NULL("p should not be null", p);
+
+    char *pp = (char *) sbrk(0);
+
+    realloc_mem(pp - 10, 40);
+
+    char *out = capture_stderr_end(s, r);
+    ASSERT_STR_MATCH("realloc_mem for unallocated heap address should log error", out,
+                     "heap corruption");
+    free(out);
+}
+
+TEST(realloc_mem_freed_pointer_logs_error) {
+    char *p = alloc_mem(32);
+    ASSERT_NOT_NULL("p should not be null", p);
+
+    int s, r;
+    capture_stderr_start(&s, &r);
+
+    free_mem(p);
+    realloc_mem(p, 40);
+
+    char *out = capture_stderr_end(s, r);
+    ASSERT_STR_MATCH("realloc_mem for freed pointer should log error", out, "double free");
+    free(out);
 }
