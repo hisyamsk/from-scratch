220314daaa4bfef5970143dd9a670cf7f0428ed1
hisyamsk <hisyamskurniawan18@gmail.com>
2025-09-03 22:21:50 +0700

    feat: add core api functions

--- hash_table/src/hash_table.c
+++ hash_table/src/hash_table.c
@@ -1,4 +1,5 @@
 #include "hash_table.h"
+#include "allocator.h"
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,68 +24,125 @@ struct ht {
     ht_config_t config;
 };
 
-static int bucket_reserve(ht_bucket_t *b, size_t new_cap) {
-    if (new_cap <= b->capacity)
+static int bucket_reserve(ht_bucket_t *bucket, size_t new_capacity) {
+    if (new_capacity <= bucket->capacity)
         return HT_OK;
 
-    size_t capacity = (b->capacity == 0 ? 4 : b->capacity * 2);
-    while (capacity < new_cap)
+    size_t capacity = (bucket->capacity == 0 ? 4 : bucket->capacity * 2);
+    while (capacity < new_capacity)
         capacity *= 2;
 
-    ht_entry_t *new_items = realloc(b->items, capacity * sizeof(ht_entry_t));
+    ht_entry_t *new_items = realloc_mem(bucket->items, capacity * sizeof(ht_entry_t));
     if (!new_items)
         return HT_ENONEM;
 
-    b->items = new_items;
-    b->capacity = capacity;
+    bucket->items = new_items;
+    bucket->capacity = capacity;
     return HT_OK;
 }
 
-static int bucket_find(const ht_bucket_t *b, unsigned hash, const void *key, size_t key_len,
+static int bucket_find(const ht_bucket_t *bucket, unsigned hash, const void *key, size_t key_len,
                        const ht_config_t *config) {
-    for (size_t i = 0; i < b->len; i++) {
-        if (b->items[i].hash == hash &&
-            config->equals(b->items[i].key, b->items[i].key_len, key, key_len)) {
+    for (size_t i = 0; i < bucket->len; i++) {
+        if (bucket->items[i].hash == hash &&
+            config->equals(bucket->items[i].key, bucket->items[i].key_len, key, key_len)) {
             return (int) i;
         }
     }
     return -1;
 }
 
-static int bucket_insert(ht_bucket_t *b, unsigned hash, void *key, size_t key_len, void *val,
+static int bucket_insert(ht_bucket_t *bucket, unsigned hash, void *key, size_t key_len, void *val,
                          const ht_config_t *config) {
-    int idx = bucket_find(b, hash, key, key_len, config);
+    int idx = bucket_find(bucket, hash, key, key_len, config);
     if (idx >= 0) {
         if (config->free_val)
-            config->free_val(b->items[idx].val);
-        b->items[idx].val = val;
+            config->free_val(bucket->items[idx].val);
+        bucket->items[idx].val = val;
         return HT_OK;
     }
 
-    int err = bucket_reserve(b, b->len + 1);
+    int err = bucket_reserve(bucket, bucket->len + 1);
     if (err != HT_OK)
         return err;
 
-    b->items[idx].hash = hash;
-    b->items[idx].key = key;
-    b->items[idx].key_len = key_len;
-    b->items[idx].val = val;
-    b->len++;
+    bucket->items[idx].hash = hash;
+    bucket->items[idx].key = key;
+    bucket->items[idx].key_len = key_len;
+    bucket->items[idx].val = val;
+    bucket->len++;
     return HT_OK;
 }
 
-static int bucket_delete(ht_bucket_t *b, unsigned hash, void *key, size_t key_len,
+static int bucket_delete(ht_bucket_t *bucket, unsigned hash, void *key, size_t key_len,
                          const ht_config_t *config) {
-    int idx = bucket_find(b, hash, key, key_len, config);
+    int idx = bucket_find(bucket, hash, key, key_len, config);
     if (idx < 0)
         return HT_ENOTFOUND;
 
-    if (config->free_val)
-        config->free_val(b->items[idx].val);
     if (config->free_key)
-        config->free_val(b->items[idx].val);
+        config->free_val(bucket->items[idx].key);
+    if (config->free_val)
+        config->free_val(bucket->items[idx].val);
 
-    b->items[idx] = b->items[b->len - 1];
-    b->len--;
+    bucket->items[idx] = bucket->items[bucket->len - 1];
+    bucket->len--;
     return HT_OK;
+}
+
+#define DEFAULT_INITIAL_CAPACITY 16
+#define DEFAULT_LOAD_FACTOR 0.75
+
+static size_t next_pow2(size_t n) {
+    size_t p = 1;
+    while (p < n)
+        p <<= 1;
+    return p;
+}
+
+ht_t *ht_create(const ht_config_t *config) {
+    if (!config)
+        return NULL;
+
+    ht_t *ht = calloc_mem(1, sizeof(ht_t));
+    if (!ht)
+        return NULL;
+
+    ht->config = *config;
+    if (ht->config.initial_capacity == 0)
+        ht->config.initial_capacity = DEFAULT_INITIAL_CAPACITY;
+
+    if (ht->config.load_factor <= 0.0)
+        ht->config.load_factor = DEFAULT_LOAD_FACTOR;
+
+    ht->capacity = next_pow2(ht->config.initial_capacity);
+    ht->size = 0;
+
+    ht->buckets = calloc_mem(ht->capacity, sizeof(ht_bucket_t));
+    if (ht->buckets) {
+        free(ht);
+        return NULL;
+    }
+
+    return ht;
+}
+
+void ht_destroy(ht_t *ht) {
+    if (!ht)
+        return;
+
+    for (size_t i = 0; i < ht->capacity; i++) {
+        ht_bucket_t *bucket = &ht->buckets[i];
+        for (size_t j = 0; j < bucket->capacity; j++) {
+            ht_entry_t *item = &bucket->items[j];
+            if (ht->config.free_key && item->key)
+                ht->config.free_key(item->key);
+            if (ht->config.free_val && item->val)
+                ht->config.free_val(item->val);
+        }
+        free_mem(bucket->items);
+    }
+
+    free_mem(ht->buckets);
+    free_mem(ht);
 }
