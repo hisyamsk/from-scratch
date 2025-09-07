cbee58094e90c1de5e2d057908486cb33dfb3b2f
hisyamsk <hisyamskurniawan18@gmail.com>
2025-09-07 21:13:09 +0700

    test: add hash_table unit tests

--- hash_table/test/hash_table.c
+++ hash_table/test/hash_table.c
@@ -3,6 +3,7 @@
 #include "test.h"
 #include "utils.h"
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
 
 static void *dup_mem(const void *src, size_t size) {
@@ -29,3 +30,62 @@ ht_config_t default_config = {
     .free_val = free_mem,
     .seed = 0xDEADABADCAFEC,
 };
+
+TEST(ht_create_destroy) {
+    ht_t *ht = ht_create(&default_config);
+    ASSERT_NOT_NULL("ht should not be null", ht);
+    ASSERT_INT_EQUAL("ht's initial size should be 0", 0, (int) ht_size(ht));
+    ht_destroy(ht);
+}
+
+TEST(ht_insert_lookup) {
+    ht_t *ht = ht_create(&default_config);
+    ASSERT_NOT_NULL("ht should not be null", ht);
+
+    ht_err_t err = ht_set(ht, "name", 4, "hisyam", 6);
+    ASSERT_INT_EQUAL("ht_set should not return error", HT_OK, err);
+
+    void *val = NULL;
+    err = ht_get(ht, "name", 4, &val);
+
+    ASSERT_INT_EQUAL("ht_set should not return error", HT_OK, err);
+    ASSERT_INT_EQUAL("ht_set should increment size", 1, (int) ht_size(ht));
+    ASSERT_NOT_NULL("val should not be null", val);
+    ASSERT_STR_EQUAL("value should match inserted", "hisyam", (char *) val, 6);
+
+    err = ht_has(ht, "name", 4);
+
+    ht_destroy(ht);
+}
+
+TEST(ht_update_value) {
+    ht_t *ht = ht_create(&default_config);
+    ASSERT_NOT_NULL("ht should not be null", ht);
+
+    ht_err_t err = ht_set(ht, "name", 4, "hisyam", 6);
+    err = ht_set(ht, "name", 4, "kurniawan", 9);
+    ASSERT_INT_EQUAL("ht_set should not return error", HT_OK, err);
+    ASSERT_INT_EQUAL("update should not increment size", 1, (int) ht_size(ht));
+
+    void *val = NULL;
+    err = ht_get(ht, "name", 4, &val);
+    ASSERT_STR_EQUAL("value should be updated", "kurniawan", (char *) val, 9);
+
+    ht_destroy(ht);
+}
+
+TEST(ht_has_and_delete) {
+    ht_t *ht = ht_create(&default_config);
+    ASSERT_NOT_NULL("ht should not be null", ht);
+
+    ht_err_t err = ht_set(ht, "name", 4, "hisyam", 6);
+    ASSERT_INT_EQUAL("ht_set should not return error", HT_OK, err);
+
+    err = ht_delete(ht, "name", 4);
+    ASSERT_INT_EQUAL("ht_delete should not return error", HT_OK, err);
+
+    err = ht_has(ht, "name", 4);
+    ASSERT_INT_EQUAL("value should be deleted", HT_ENOTFOUND, err);
+
+    ht_destroy(ht);
+}
