3e9a7a8de0f5f415da6b447a9de64f53b4c7f9e5
hisyamsk <hisyamskurniawan18@gmail.com>
2025-09-08 07:13:33 +0700

    test: add more unit tests

--- hash_table/test/hash_table.c
+++ hash_table/test/hash_table.c
@@ -89,3 +89,68 @@ TEST(ht_has_and_delete) {
 
     ht_destroy(ht);
 }
+
+TEST(ht_resize_trigger) {
+    ht_t *ht = ht_create(&default_config);
+    ASSERT_NOT_NULL("ht should not be null", ht);
+
+    size_t old_cap = ht_capacity(ht);
+    int insert_count = (int) (old_cap * 0.75) + 1;
+
+    for (int i = 0; i < insert_count; i++) {
+        char key[16];
+        snprintf(key, sizeof(key), "k%d", i);
+        ht_set(ht, key, strlen(key), "val", 3);
+    }
+
+    ASSERT_INT_EQUAL("capacity should grow", 32, (int) ht_capacity(ht));
+    ASSERT_UINT_EQUAL("size should equal number of inserts", insert_count, (int) ht_size(ht));
+
+    ht_destroy(ht);
+}
+
+TEST(ht_clear_zero_size) {
+    ht_t *ht = ht_create(&default_config);
+    ASSERT_NOT_NULL("ht should not be null", ht);
+
+    ht_set(ht, "name", 4, "hisyam", 6);
+    ht_set(ht, "country", 7, "indonesia", 9);
+    ht_set(ht, "city", 4, "jakarta", 7);
+
+    ASSERT_TRUE("size should be 3", 3 == ht_size(ht));
+
+    ht_clear(ht);
+
+    ASSERT_TRUE("size should be 0", 0 == ht_size(ht));
+    ASSERT_INT_EQUAL("deleted key should not exists", HT_ENOTFOUND, ht_has(ht, "name", 4));
+
+    ht_destroy(ht);
+}
+
+TEST(ht_iterator) {
+    ht_t *ht = ht_create(&default_config);
+    ASSERT_NOT_NULL("ht should not be null", ht);
+    int val1 = 1, val2 = 2, val3 = 3;
+
+    ht_set(ht, "a", 1, (void *) &val1, sizeof(int));
+    ht_set(ht, "b", 1, (void *) &val2, sizeof(int));
+    ht_set(ht, "c", 1, (void *) &val3, sizeof(int));
+
+    ASSERT_TRUE("size should be 3", 3 == ht_size(ht));
+
+    ht_iter_t hi = ht_iter_begin(ht);
+    void *key;
+    size_t key_len;
+    void *val;
+    int count = 0;
+
+    while (ht_iter_next(&hi, &key, &key_len, &val)) {
+        count++;
+        ASSERT_NOT_NULL("key should not be null", key);
+        ASSERT_NOT_NULL("val should not be null", val);
+    }
+
+    ASSERT_INT_EQUAL("iterator should visit all elements", 3, count);
+
+    ht_destroy(ht);
+}
